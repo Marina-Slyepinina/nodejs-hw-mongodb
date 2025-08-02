bc39a1f769917a964a77e6c77ec24d306018d2d3
Marina <marina.slepinina3@gmail.com>
2025-08-03 00:44:26 +0300

    fix response in patchContactController

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -49,7 +49,7 @@ export const patchContactController = async (req, res, next) => {
     res.status(200).json({
 		status: 200,
 		message: "Successfully patched a contact!",
-		data: contact,        
+		data: contact.value,        
     });
 };
 
