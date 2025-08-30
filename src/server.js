6a001fa86d981e77625137395330f134089a291f
Marina <marina.slepinina3@gmail.com>
2025-08-30 14:30:11 +0300

    hw7 swagger

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { errorHandler } from "./middlewares/errorHandler.js";
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import cookieParser from "cookie-parser";
 import { UPLOAD_DIR } from "./constants/constants.js";
+import { swaggerDocs } from "./middlewares/swaggerDocs.js";
 
 const PORT = Number(getEnvVar("PORT", "3000"));
 
@@ -29,6 +30,7 @@ export const setupServer = () => {
     );
 
     app.use("/uploads", express.static(UPLOAD_DIR));
+    app.use("/api-docs", swaggerDocs());    
 
     app.use(authRouter);
     app.use(contactsRouter);
